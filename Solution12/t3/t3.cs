15f337f1d1e6625ea1b76c2fb004105560c435fc
glenn swonk <glenn@swonk.com>
2014-03-28 15:27:50 -0700

    aa

---
+++ Solution12/t3/t3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t3
+{
+    class t3
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
